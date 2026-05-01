94905e60100e43a98b1331a3b95e8b5c92603c58
Josimar Vera <josimarveram@gmail.com>
2026-05-01 01:23:07 -0500

    ajustes conexion java

--- src/main/java/com/sdrerc/infrastructure/database/OracleConnection.java
+++ src/main/java/com/sdrerc/infrastructure/database/OracleConnection.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
  * @author David
  */
 public class OracleConnection {
-    private static final String URL = "jdbc:oracle:thin:@//localhost:1521/XEPDB1";
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521/xe";
     private static final String USER = "system";
     private static final String PASSWORD = "satrapa12345";
 
