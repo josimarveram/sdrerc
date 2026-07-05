2a02b1900435e206284b727340d085c12b50718e
Josimar Vera <josimarveram@gmail.com>
2026-07-05 11:29:16 -0500

    ui: align analysis blocks with registro layout

--- src/main/java/com/sdrerc/ui/views/analisis/JPanelAnalisisV2.java
+++ src/main/java/com/sdrerc/ui/views/analisis/JPanelAnalisisV2.java
@@ -431,11 +431,11 @@ public class JPanelAnalisisV2 extends JPanel {
     }
 
     private JPanel crearCentro() {
-        JPanel contenidoPrincipal = new JPanel(new BorderLayout(8, 8));
+        JPanel contenidoPrincipal = new JPanel(new BorderLayout(4, 4));
         contenidoPrincipal.setOpaque(false);
         contenidoPrincipal.add(crearHeader(), BorderLayout.NORTH);
 
-        JPanel contenidoOperativo = new JPanel(new BorderLayout(8, 8));
+        JPanel contenidoOperativo = new JPanel(new BorderLayout(4, 4));
         contenidoOperativo.setOpaque(false);
         contenidoOperativo.add(crearBuscador(), BorderLayout.NORTH);
         contenidoOperativo.add(crearBandeja(), BorderLayout.CENTER);
